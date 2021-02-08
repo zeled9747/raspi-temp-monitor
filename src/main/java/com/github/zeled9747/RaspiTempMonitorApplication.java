6fa30c5ce772828cdddd6075c3c8133ea527120a
Zele <zeled9747@gmail.com>
2021-02-08 21:06:52 +0100

    first commit

---
+++ src/main/java/com/github/zeled9747/RaspiTempMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.github.zeled9747;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspiTempMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspiTempMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/zeled9747/RaspiTempMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zeled9747;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaspiTempMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
