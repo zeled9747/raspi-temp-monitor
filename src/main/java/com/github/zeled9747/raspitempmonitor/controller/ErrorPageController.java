5efd4779c219efc22b880961d52eef27163f22c6
Zele <zeled9747@gmail.com>
2021-02-15 20:28:51 +0100

    error page

--- src/main/java/com/github/zeled9747/raspitempmonitor/controller/ErrorPageController.java
+++ src/main/java/com/github/zeled9747/raspitempmonitor/controller/ErrorPageController.java
@@ -1,11 +1,32 @@
 package com.github.zeled9747.raspitempmonitor.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.reactive.function.server.ServerRequest;
 
 @Controller
 public class ErrorPageController implements ErrorController {
+
+    @Autowired
+    DefaultErrorAttributes errorAttributes;
+
+    @RequestMapping("/error")
+    public String error(Model model, WebRequest request) {
+        Map<String, Object> error = this.errorAttributes.getErrorAttributes((ServerRequest) request,
+                ErrorAttributeOptions.defaults());
+        model.addAllAttributes(error);
+        System.out.println(error);
+        return "error/error";
+    }
 
     @GetMapping("/notraspberry")
     public String notRaspberry() {
